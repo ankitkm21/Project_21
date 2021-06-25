f84b70f7d97a41a9a1e6c7fccfb938edffa67b03
ankitkm21 <30433288+ankitkm21@users.noreply.github.com>
2021-06-25 10:29:19 +0530

    Update code.js

--- code.js
+++ code.js
@@ -16,14 +16,14 @@ function setup() {
     h = windowHeight;
     createCanvas(w, h);
 
-    wight = Math.round(random(30,52));
+    weight = Math.round(random(30,52));
     thickness = Math.round(random(22,83));
     speed = Math.round(random(223,321));
     //v = Math.round(random(50,60));
     console.log(v);
 
     bullet = createSprite(80,h/2,20,20);
-    bullet.addImage(bullet2);
+    // bullet.addImage(bullet2);
     bullet.addImage(bulletImage);
     bullet.debug = true;
     bullet.scale = 0.42;
@@ -45,18 +45,18 @@ function draw() {
 
     if (touching(wall,bullet)) {
         var d = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
-        
+        console.log(d)
     if (d < 10) {
         r.visible = true;
         bullet.velocityX = 0;
-        bullet.changeImage(bullet2);
+        bullet.addImage(bullet2);
 
     } else if (d > 10) {
         g.visible = true;
         wall.visible = false;
         bullet.velocityX = 0;
-        wallSound.play();
-        bullet.changeImage(bullet2);
+        // wallSound.play();
+        bullet.addImage(bullet2);
     }
     }
 
@@ -64,7 +64,9 @@ function draw() {
 }
 
 function touching(a,b) {
+
     if (a.x - b.x < a.width/2 + b.width/2) {
+        
         return true;
     } else {
         return false;
